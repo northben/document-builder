75127bc498591767388a7671205a2623077013b9
Ben Northway <northben@pixelchef.net>
2019-09-13 12:50:28 -0400

    adding the front end javascript!

---
+++ appserver/static/builder.js
@@ -0,0 +1,82 @@
+function s2ab(s) {
+  var buf = new ArrayBuffer(s.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+  return buf;
+}
+
+function objectifyForm (formArray) {
+  // serialize data function
+
+  var returnArray = {}
+  for (var i = 0; i < formArray.length; i++) {
+    if (formArray[i]['type'] == 'submit') {
+      continue
+    }
+    returnArray[formArray[i]['name']] = formArray[i]['value']
+  }
+  return returnArray
+}
+
+require([
+  'underscore',
+  'jquery',
+  'splunkjs/mvc',
+  'splunkjs/mvc/tableview',
+  'splunkjs/mvc/simplexml/ready!'
+], function (_, $, mvc, TableView) {
+  var service = mvc.createService()
+
+  // $('#merge_document').on('click', function () {
+  //   console.log('clicked merge document!')
+  // })
+
+  var theForm = $('#the_form')
+
+  theForm.submit(function (event) {
+    event.preventDefault()
+
+    var tasks = $('#tasks table').tableToJSON()
+    var issues = $('#issues table').tableToJSON()
+
+    var data = objectifyForm(document.querySelector('#the_form'))
+    data["merge_rows"] = []
+    data["merge_rows"].push({"task_dates": tasks })
+    data["merge_rows"].push({"issue_status": issues })
+    data["merge_rows"] = JSON.stringify(data["merge_rows"])
+
+    function saveOrOpenBlob(blob, fileName) {
+        window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+        window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function (fs) {
+            fs.root.getFile(fileName, { create: true }, function (fileEntry) {
+                fileEntry.createWriter(function (fileWriter) {
+                    fileWriter.addEventListener("writeend", function () {
+                        window.location = fileEntry.toURL();
+                    }, false);
+                    fileWriter.write(blob, "_blank");
+                }, function () { });
+            }, function () { });
+        }, function () { });
+    }
+
+    service.post('/services/merge_document', data, function (err, response) {
+      if (err) {
+        console.log('error: ', err)
+      } else if (response.status === 200) {
+        console.log('response 200 ')
+
+        var blob = new Blob([s2ab(atob(response.data))], {
+          type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        });
+
+        var link=document.createElement('a');
+        link.href=window.URL.createObjectURL(blob);
+
+        link.download="output.docx";
+        link.click();
+
+      }
+    })
+  })
+
+})
